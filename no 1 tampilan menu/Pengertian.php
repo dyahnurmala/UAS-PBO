afb8ab770124a707a57d452018cf29ae23f36fcf
yoongi's future wife <130167211+dyahnurmala@users.noreply.github.com>
2024-01-25 21:41:29 +0700

    first commit

---
+++ no 1 tampilan menu/Pengertian.php
@@ -0,0 +1,5 @@
+Pengertian Migration, Model dan Resource
+
+1. Migration = Membantu mengelola database secara efisien menggunakan kode, membuat (create), mengubah(edit) dan menghapus (delete) struktur tabel dan kolom pada database dengan cepat
+2. Model = Merupakan representasi dari tabel database dapat melakukan operasi database membuat, membaca mengupdate dan menghapus data
+3. Resource = Memudahkan dalam mengelola API yang datanya sudah dibuat dalam bentuk JSON
